7343806f3214b5e12dbe0509494aa39fdddcf3c0
Benjamin Jasper <jasper.benni@gmail.com>
2022-09-08 23:01:41 +0200

    feat: added axiom log file

--- go.mod
+++ go.mod
@@ -6,9 +6,13 @@ require (
 	entgo.io/contrib v0.3.2
 	entgo.io/ent v0.11.3-0.20220830071904-3b1b75b9d7a9
 	github.com/99designs/gqlgen v0.17.16
+	github.com/axiomhq/axiom-go v0.11.0
 	github.com/cnf/structhash v0.0.0-20201127153200-e1b16c1ebc08
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/hashicorp/go-multierror v1.1.1
+	github.com/joho/godotenv v1.4.0
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mitchellh/mapstructure v1.5.0
 	github.com/segmentio/fasthash v1.0.3
 	github.com/vektah/gqlparser/v2 v2.5.0
 	github.com/vmihailenco/msgpack/v5 v5.0.0-beta.9
@@ -20,17 +24,20 @@ require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/klauspost/compress v1.15.8 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
--- logging/logging.go
+++ logging/logging.go
@@ -1,59 +1,212 @@
 package logging
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/axiomhq/axiom-go/axiom"
 	"log"
 	"os"
+	"time"
 )
+
+const logFile = "./tmp/logs.log"
 
 type Logger struct {
 	logger *log.Logger
 }
 
+type Message struct {
+	Type    string    `json:"type"`
+	Message string    `json:"message"`
+	Time    time.Time `json:"time"`
+}
+
+func (m *Message) String() string {
+	return fmt.Sprintf("%s [%s] %s\n", m.Time.Format("2006-01-02 15:04:05"), m.Type, m.Message)
+}
+
 func NewLogger() *Logger {
-	file, err := os.OpenFile("logs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	logger := log.New(file, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger := log.New(file, "", 0)
 
-	return &Logger{
+	loggerObject := &Logger{
 		logger: logger,
 	}
+
+	// Trigger upload every minute
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			loggerObject.uploadLog()
+		}
+	}()
+
+	return loggerObject
 }
 
 func (l *Logger) Debug(input string) {
-	message := fmt.Sprintf("[DEBUG] %s\n", input)
+	m := Message{
+		Type:    "DEBUG",
+		Message: input,
+		Time:    time.Now(),
+	}
 
-	l.logger.Printf(message)
+	message := fmt.Sprintf("[DEBUG] %s\n", input)
+	l.messageToJson(&m)
+
 	log.Print(message)
 }
 
 func (l *Logger) Info(input string) {
-	message := fmt.Sprintf("[INFO] %s\n", input)
+	m := Message{
+		Type:    "INFO",
+		Message: input,
+		Time:    time.Now(),
+	}
 
-	l.logger.Printf(message)
+	message := fmt.Sprintf("[INFO] %s\n", input)
+	l.messageToJson(&m)
+
 	log.Print(message)
 }
 
 func (l *Logger) Warn(input string) {
-	message := fmt.Sprintf("[WARN] %s\n", input)
+	m := Message{
+		Type:    "WARN",
+		Message: input,
+		Time:    time.Now(),
+	}
 
-	l.logger.Printf(message)
+	message := fmt.Sprintf("[WARN] %s\n", input)
+	l.messageToJson(&m)
+
 	log.Print(message)
 }
 
 func (l *Logger) Error(input string) {
-	message := fmt.Sprintf("[ERROR] %s\n", input)
+	m := Message{
+		Type:    "ERROR",
+		Message: input,
+		Time:    time.Now(),
+	}
 
-	l.logger.Printf(message)
+	message := fmt.Sprintf("[ERROR] %s\n", input)
+	l.messageToJson(&m)
+
 	log.Print(message)
 }
 
 func (l *Logger) Fatal(input error) {
-	message := fmt.Sprintf("[FATAL] %s\n", input)
+	m := Message{
+		Type:    "FATAL",
+		Message: input.Error(),
+		Time:    time.Now(),
+	}
 
-	l.logger.Printf(message)
+	message := fmt.Sprintf("[FATAL] %s\n", input)
+	l.messageToJson(&m)
+
 	log.Fatal(message)
+}
+
+func (l *Logger) messageToJson(message *Message) {
+	// Marshal to JSON string
+	jsonMessage, err := json.Marshal(message)
+	if err != nil {
+		return
+	}
+
+	// Write to file
+	l.logger.Println(string(jsonMessage))
+}
+
+func (l *Logger) uploadLog() {
+	f, err := os.Open(logFile)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	// If file is empty, return
+	fileInfo, err := f.Stat()
+	if err != nil {
+		return
+	}
+
+	if fileInfo.Size() == 0 {
+		return
+	}
+
+	log.Println("Uploading log file to Axiom")
+
+	// Rename the file to prevent loss of data
+	renamedFile := fmt.Sprintf("./tmp/logs-%s.log", time.Now().Format("2006-01-02"))
+
+	err = os.Rename(logFile, renamedFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l.logger.SetOutput(file)
+
+	// Upload the file to a remote server
+	l.uploadLogToAxiom(renamedFile)
+}
+
+func (l *Logger) uploadLogToAxiom(file string) {
+	// Upload the file to Axiom
+	dataset := os.Getenv("AXIOM_DATASET")
+	if dataset == "" {
+		return
+	}
+
+	// 1. Open the file to ingest.
+	f, err := os.Open(file)
+	if err != nil {
+		l.Fatal(err)
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	// 2. Wrap it in a gzip enabled reader.
+	r, err := axiom.GzipEncoder(f)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	// 3. Initialize the Axiom API client.
+	client, err := axiom.NewClient()
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	// 4. Ingest ⚡
+	// Note the JSON content type and Gzip content encoding being set because
+	// the client does not auto sense them.
+	res, err := client.Datasets.Ingest(context.Background(), dataset, r, axiom.NDJSON, axiom.Gzip, axiom.IngestOptions{})
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	// 5. Make sure everything went smoothly.
+	for _, fail := range res.Failures {
+		l.Error(fail.Error)
+	}
+
+	// 6. Remove the file
+	err = os.Remove(file)
+	if err != nil {
+		l.Fatal(err)
+	}
 }
